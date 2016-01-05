1c941dfc905b3b7f34dcdd45b13ec04cec3635ef
Jim Geurts <jim@biacreations.com>
2016-01-05 12:56:50 -0600

    Register for 64bit systems

--- Sources/KinectCam/install.bat
+++ Sources/KinectCam/install.bat
@@ -1,5 +1,5 @@
-@%windir%\Microsoft.NET\Framework\v4.0.30319\ngen.exe install BaseClassesNET.dll
-@%windir%\Microsoft.NET\Framework\v4.0.30319\ngen.exe install KinectCam.dll
+@%windir%\Microsoft.NET\Framework64\v4.0.30319\ngen.exe install BaseClassesNET.dll
+@%windir%\Microsoft.NET\Framework64\v4.0.30319\ngen.exe install KinectCam.dll
 
-@%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe /nologo BaseClassesNET.dll
-@%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe KinectCam.dll /nologo /codebase /tlb: KinectCam.tlb
+@%windir%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe /nologo BaseClassesNET.dll
+@%windir%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe KinectCam.dll /nologo /codebase /tlb: KinectCam.tlb
--- Sources/KinectCam/uninstall.bat
+++ Sources/KinectCam/uninstall.bat
@@ -1,5 +1,5 @@
-@%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe /unregister /nologo BaseClassesNET.dll
-@%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe /unregister /nologo KinectCam.dll
+@%windir%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe /unregister /nologo BaseClassesNET.dll
+@%windir%\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe /unregister /nologo KinectCam.dll
 
-@%windir%\Microsoft.NET\Framework\v4.0.30319\ngen.exe uninstall BaseClassesNET.dll
-@%windir%\Microsoft.NET\Framework\v4.0.30319\ngen.exe uninstall KinectCam.dll
+@%windir%\Microsoft.NET\Framework64\v4.0.30319\ngen.exe uninstall BaseClassesNET.dll
+@%windir%\Microsoft.NET\Framework64\v4.0.30319\ngen.exe uninstall KinectCam.dll
